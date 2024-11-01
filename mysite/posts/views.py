4f3d992673cd10cc77b44fdc07ac4752785d2443
Your Name <you@example.com>
2024-11-01 21:29:51 +0500

    Update

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post, Genre
 
 def post_list(request):
-
-    posts = Post.objects.all()
+    genre_id = request.GET.get('genre_id')
+    if genre_id != None:
+        posts = Post.objects.filter(genre__id=genre_id)
+    else:
+        posts = Post.objects.all()
+    
     genres = Genre.objects.all()
     context = {
         "posts": posts,
@@ -21,3 +25,26 @@ def post(request, id):
     }
 
     return render(request, 'posts/post.html', context)
+
+def create_post(request):
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        genre = request.POST.get('genre')
+        image = request.FILES.get('image')
+
+        genre = Genre.objects.get(id=genre)
+        Post.objects.create(title=title,
+                            content=content,
+                            genre=genre,
+                            image=image)
+        return redirect('post_list')
+
+    genres = Genre.objects.all()
+
+    context = {
+        "genres": genres
+    }
+
+    return render(request, 'posts/post_create.html', context)
